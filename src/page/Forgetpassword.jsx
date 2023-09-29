4453c3b260b17b3857b8524e2bbbced80399ce65
A-Tushar <mishurahman2022@gmail.com>
2023-09-29 20:27:12 +0600

    Password Reset Done.
    used a component and firebase rest email set.

--- src/page/Forgetpassword.jsx
+++ src/page/Forgetpassword.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from 'react-toastify';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 const Forgetpassword = () => {
+  const auth = getAuth();
+  let [email, setemail]= useState("")
+  let nevigate = useNavigate()
+
+  
+
+  let handleforgetpass= ()=>{
+
+  sendPasswordResetEmail(auth, email)
+  .then(() => {
+    // Password reset email sent!
+    toast.success(' Reset Email Sent !', {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+      setTimeout(() => {
+        nevigate("/login")
+      }, 1000);
+  })
+  .catch((error) => {
+    toast.error(' Something went Wrong !', {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+  });
+  }
+
   return (
-    <div>Forgetpassword</div>
+    <div className="forgetpage">
+      <div className="forgetbox">
+        <h3> Reset You Password !</h3>
+      <TextField onChange={(e)=>setemail(e.target.value)} id="standard-basic" label="Your Email" variant="standard" /> <br />
+      <Button onClick={handleforgetpass} variant="contained">Reset</Button>
+      </div>
+    </div>
   )
 }
 
