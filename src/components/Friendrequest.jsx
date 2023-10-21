4feff3255e22d6042636547ce07be9ac5886cc74
A-Tushar <mishurahman2022@gmail.com>
2023-10-22 03:47:56 +0600

    Friend Request
    Friend request part is done ,

--- src/components/Friendrequest.jsx
+++ src/components/Friendrequest.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from './Image'
 import gimg from '../assets/img.png'
+import { getDatabase, ref,onValue, set,push, remove } from "firebase/database";
+import { useSelector } from 'react-redux';
+
 
 const Friendrequest = () => {
+  const db = getDatabase();
+  let userinfo = useSelector((state)=>state.logedUser.value);
+  let [requeslist,setRequestlist]=useState([])
+
+
+  useEffect(()=>{
+    
+    const userRef = ref(db, 'friendrequest');
+    onValue(userRef, (snapshot) => {
+    let arr=[]
+    snapshot.forEach(item=>{
+      if(item.val().whoreceiveid==userinfo.uid){
+        arr.push({...item.val(), frid:item.key});
+      }
+      // console.log(item.val());
+      // console.log("loinuser",userinfo);
+      // console.log(arr);
+      
+    })
+    setRequestlist(arr);
+    });
+    },[]);
+
+    let handlereject =(item)=>{
+      remove( ref(db, 'friendrequest/'+item.frid))
+    }
+
   return (
     <>
     <div className="box">
         <h2>Friend Requests</h2>
-        {/* <div className='list'>
+        {requeslist.map(item=>
+        
+        <div className='list'>
         <Image src={gimg}/>
-        <h3>Friends Forever</h3>
-        <button>JOIN</button>
-        </div> */}
+        <h3>{item.whosendname}</h3>
+        <button>Accept</button>
+        <p onClick={()=>handlereject(item)} className='rejectbtn'>Reject</p>
+        </div>
+        
+        )}
+        
     </div>
     </>
   )
--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import Image from './Image'
-import gimg from '../assets/img.png'
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref,onValue, set,push } from "firebase/database";
 import { useSelector } from 'react-redux';
 
 const Userlist = () => {
   const db = getDatabase();
-  let userinfo = useSelector((state)=>state.logedUser.value)
+  let userinfo = useSelector((state)=>state.logedUser.value);
+  let [requestlist,setRequestlist]=useState([]);
   let [users,SetUsers]=useState([]);
 
   useEffect(()=>{
@@ -16,13 +16,37 @@ const Userlist = () => {
   let arr=[]
   snapshot.forEach(item=>{
     if(userinfo.uid != item.key){
-      arr.push(item.val());
+      arr.push({...item.val(), userid:item.key});
     }
     
   })
   SetUsers(arr);
   });
-  },[])
+  },[]);
+
+  let handlefriendrequest =(item)=>{
+
+    set(push(ref(db, 'friendrequest')), {
+      whosendid : userinfo.uid,
+      whosendname : userinfo.displayName,
+      whoreceiveid: item.userid,
+      whoreceivename:item.username
+    });
+  };
+
+  useEffect(()=>{
+    
+    const userRef = ref(db, 'friendrequest');
+    onValue(userRef, (snapshot) => {
+    let arr=[]
+    snapshot.forEach(item=>{
+      arr.push(item.val().whoreceiveid+item.val().whosendid);      
+    })
+
+    setRequestlist(arr);
+
+    });
+    },[]);
 
   return (
     <>
@@ -32,7 +56,12 @@ const Userlist = () => {
         <div className='list'>
         <Image className={"boximg"} src={item.profile_picture}/>
         <h3>{item.username}</h3>
-        <button>Add Friend</button>
+        {requestlist.includes(item.userid+userinfo.uid)||requestlist.includes(userinfo.uid+item.userid)?
+      <p className='rejectbtn'>Pending</p>
+      :
+      <button onClick={()=>handlefriendrequest(item)}>Add Friend</button>
+      }
+        
         </div>
 
         )}
